a4483e1d2cdba46427134791bd5a0b13dcf0f42e
DanielZ707 <92012434+DanielZ707@users.noreply.github.com>
2023-01-13 13:20:15 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'wekexinyylgjsf';
-const PASSWORD = '976b3e5638882895e07fab12248338b9e9dd47c0f2bcc621f74d8828ecbf605f';
-const HOST = 'ec2-34-243-101-244.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'dfs4vabvnl7398';
